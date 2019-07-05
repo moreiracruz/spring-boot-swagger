e5d0e1560c0bceb277220f2defe3efa38ed73a41
Paulo André Moreira Cruz <paulo.cruz@itlab.com.br>
2019-07-05 15:36:36 -0300

    SwaggerConfig

---
+++ src/main/java/io/com/github/pamcruz/springbootswagger/model/Greeting.java
@@ -0,0 +1,14 @@
+package io.com.github.pamcruz.springbootswagger.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+	
+	private String message;
+
+}
